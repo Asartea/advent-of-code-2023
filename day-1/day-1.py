6e1bc5471d1fb5ba062ed30f0c1ae12c71203b99
Asartea <76259120+Asartea@users.noreply.github.com>
2023-12-02 08:01:39 +0100

    cleanup day-1

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -1,14 +1,22 @@
-def main():
-    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+def part_1(data):
+    total_sum = 0
+    for line in data:
+        found_digits = []
+        for char in line.split(""):
+            if char.isdigit():
+                found_digits.append(int(char))
+        calibration_value = found_digits[0] * 10 + found_digits[-1]
+        total_sum += calibration_value
+
+    return total_sum
+
+def part_2(data):
     digit_words = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
     total_sum = 0
-    in_file = open("day-1-input.txt")
-    data = in_file.readlines()
-    in_file.close()
-    for line in in_file:
+    for line in data:
         found_digits = []
         for i, char in enumerate(line):
-            if char in digits:
+            if char.isdigit():
                 found_digits.append(int(char))
             for word, digit in digit_words.items():
                 if line[i:].startswith(word):
@@ -16,5 +24,15 @@ def main():
         calibration_value = found_digits[0] * 10 + found_digits[-1]
         total_sum += calibration_value
     print(total_sum)
+
+def main():
+    in_file = open("day-1-input.txt")
+    data = in_file.readlines()
+    in_file.close()
+    part_1_sum = part_1(data)
+    print(part_1_sum)
+    part_2_sum = part_2(data)
+    print(part_2_sum)
+
 
 main()
